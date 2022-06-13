95eb36e89eb780170ce9d096a44d11e8d4527a4d
tej <tej@pop-os.localdomain>
2022-06-13 19:44:08 -0400

    Add responsive font size to theme for h1

--- Webapp/frontend/src/App.js
+++ Webapp/frontend/src/App.js
@@ -6,13 +6,13 @@ import React from 'react'
 import './App.css'
 
 import 'fontsource-roboto'
-import {  Grid } from '@mui/material'
+import { Grid } from '@mui/material'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { responsiveFontSizes } from '@mui/material'
 import { CssBaseline } from '@mui/material'
 import Button from '@mui/material/Button'
 import { Typography } from '@mui/material'
-let theme = createTheme({
+const defaultTheme = createTheme({
   palette: {
     primary: {
       main: '#091425',
@@ -22,24 +22,31 @@ let theme = createTheme({
 
     },
     secondary: {
-      
+
       main: '#e0164a',
       contrastText: '#E2DEDE'
     }
   },
-  
-  typography:{
-
-  }
 })
 
-theme = responsiveFontSizes(theme);
+const {breakpoints, typography: { pxToRem } } = defaultTheme
+let theme = {...defaultTheme,
+  typography:{
+    h1:{fontSize: '1.9rem',
+  '@media (min-width:600px)': {
+    fontSize: '2.5rem',
+  },
+  [defaultTheme.breakpoints.up('md')]: {
+    fontSize: '3.4rem',
+  },
+}}}
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Typography variant='h1'>Automated Mail Sorter</Typography>
-      <Button variant= "contained" color = "primary">Hello world</Button>
+      <Button variant="contained" color="primary">Hello world</Button>
     </ThemeProvider>
 
   )

2ed3d4ae4674fc889f7d19a694feee7bd3ee14b2
tej <tej@pop-os.localdomain>
2022-06-13 23:43:45 -0400

    Add mock UI

--- Webapp/frontend/src/App.js
+++ Webapp/frontend/src/App.js
@@ -3,6 +3,7 @@
 
 
 import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 import 'fontsource-roboto'
@@ -12,13 +13,13 @@ import { responsiveFontSizes } from '@mui/material'
 import { CssBaseline } from '@mui/material'
 import Button from '@mui/material/Button'
 import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { Paper } from '@mui/material'
 const defaultTheme = createTheme({
   palette: {
     primary: {
       main: '#091425',
       contrastText: '#E2DEDE'
-
-
 
     },
     secondary: {
@@ -29,24 +30,67 @@ const defaultTheme = createTheme({
   },
 })
 
-const {breakpoints, typography: { pxToRem } } = defaultTheme
-let theme = {...defaultTheme,
-  typography:{
-    h1:{fontSize: '1.9rem',
-  '@media (min-width:600px)': {
-    fontSize: '2.5rem',
-  },
-  [defaultTheme.breakpoints.up('md')]: {
-    fontSize: '3.4rem',
-  },
-}}}
+const { breakpoints, typography: { pxToRem } } = defaultTheme
+let theme = {
+  ...defaultTheme,
+  typography: {
+    h1: {
+      fontSize: '1.5rem',
+      '@media (min-width:600px)': {
+        fontSize: '2.4rem',
+      },
+      [defaultTheme.breakpoints.up('md')]: {
+        fontSize: '3rem',
+      },
+    }
+  }
+}
+
 
 
 function App() {
+  const [val1, setVal1] = useState(0);
+  const [val2, setVal2] = useState(0);
+  const [val3, setVal3] = useState(0);
+
+  function f1() {
+    setVal1(val1 + 1);
+  }
+  function f2() {
+    setVal2(val2 + 1);
+  }
+  function f3() {
+    setVal3(val3 + 1);
+  }
   return (
     <ThemeProvider theme={theme}>
-      <Typography variant='h1'>Automated Mail Sorter</Typography>
-      <Button variant="contained" color="primary">Hello world</Button>
+      <Grid container justifyContent="center" textAlign="center">
+        <Typography variant='h1'>Automated Mail Sorting</Typography>
+
+      </Grid>
+      <Grid container justifyContent="center">
+        <Paper elevation={10} style={{ display: 'flex', alignItems: 'center', justifyContent:"center",height: 75, width: 75, backgroundColor: "#091425" }}>
+          <Typography variant='h6' color="white">Start Sorting</Typography></Paper>
+      </Grid>
+      <Grid container padding="25px" justifyContent="space-around">
+      <Grid item >
+          <Paper elevation={10} style={{ display: 'flex',flexDirection:"column", alignItems: 'center', justifyContent: 'center', height: 75, width: 75, backgroundColor: "#e0164a" }}>
+          <Typography variant='h5' color="white">ON</Typography>
+            <Button onClick={f1} variant="contained" color='secondary'>{val1}</Button></Paper>
+        </Grid>
+        <Grid item>
+          <Paper elevation={10} style={{ display: 'flex',flexDirection:"column", alignItems: 'center', justifyContent: 'center', height: 75, width: 75, backgroundColor: "#e0164a" }}>
+          <Typography variant='h5' color="white">AB</Typography>
+            <Button onClick={f2} variant="contained" color='secondary'>{val2}</Button></Paper>
+        </Grid>
+        <Grid item>
+          <Paper elevation={10} style={{ display: 'flex',flexDirection:"column", alignItems: 'center', justifyContent: 'center', height: 75, width: 75, backgroundColor: "#e0164a" }}>
+          <Typography variant='h5' color="white">BC</Typography>
+            <Button onClick={f3} variant="contained" color='secondary'>{val3}</Button></Paper>
+        </Grid>
+      </Grid>
+
+
     </ThemeProvider>
 
   )

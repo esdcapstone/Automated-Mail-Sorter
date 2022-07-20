3b1f668265239926987d4fd47d2efadd273765b9
tej <tej@pop-os.localdomain>
2022-07-20 16:51:11 -0400

    Add users routes, create functionality to add users using fastapi format

--- Webapp/backend/app/utils/user_utils.py
+++ Webapp/backend/app/utils/user_utils.py
@@ -1,7 +1,7 @@
 import app.models.user_model as user_model
 import app.schemas.user_schema as user_schema
 from sqlalchemy.orm import Session
-
+from passlib.context import CryptContext
 # Get user by id
 
 
@@ -20,12 +20,26 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(user_model.User).offset(skip).limit(limit).all()
 
 
+# Encrypt the password using bcrypt algorithm
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Verify if password is correct
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(plain_password):
+    return pwd_context.hash(plain_password)
+
+
 # Create user
 def create_user(db: Session, user: user_schema.UserCreate):
-    fake_hashed_password = user.password + "random_hashing"
-    print(fake_hashed_password)
+    hashed_password = get_password_hash(user.password)
+    print(hashed_password)
     db_user = user_model.User(
-        email=user.email, hashed_password=fake_hashed_password)
+        email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
